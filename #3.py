d3e25ae0cb9d8dc62efdb880977a22906c4bb00a
ahmadalvin92 <71621277+ahmadalvin92@users.noreply.github.com>
2020-12-31 20:21:25 +0700

    Add files via upload

---
+++ #3.py
@@ -0,0 +1,11 @@
+file = open("file.txt" , "r")
+data = file.readlines()
+dataMHS = {}
+for i in range(len(data)) :
+    Mhs = data[i]
+    a,b,c = Mhs.split('|')
+    a,b,c= a,b,c.replace('\n', '')
+    dataMahasiswa = {'nim' : a, 'nama' : b, 'alamat' : c}
+    dataMHS[a] = dataMahasiswa
+print(dataMHS)
+file.close
